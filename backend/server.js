43b474a5a844c20e0f86ad3293d9f13a91a1b17d
kimbeomjin95 <kimbeomjin95@github.com>
2022-01-31 21:19:18 +0900

    env 환경변수 적용

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-const express  = require('express');
+const express  = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
 const _ = require('lodash')
 
+dotenv.config()
+
 const app = express()
-const port = 5000
 
 /* 메인페이지 조회 */
 app.get('/', (req, res) => {
@@ -22,4 +24,7 @@ app.get('/api/products/:id', (req, res) => {
 	res.json(product)
 })
 
-app.listen(port, console.log('Server running on port 5000'))
+// env 환경변수 get
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
