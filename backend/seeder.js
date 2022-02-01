a31c433433fc42a28523bd6c5f760a192e0c1507
kimbeomjin95 <kimbeomjin95@github.com>
2022-02-01 12:15:27 +0900

    data seeder script

---
+++ backend/seeder.js
@@ -0,0 +1,64 @@
+/* 데이터를 가져오기 위해 실행할 수 있는 별도의 스크립트 */
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import users from './data/users.js';
+import products from './data/products.js';
+import User from './models/userModel.js';
+import Product from './models/productModel.js';
+import Order from './models/orderModel.js';
+import connectDb from './config/db.js';
+
+// .env 파일안에 정보 get
+dotenv.config();
+
+connectDb();
+
+/* data insert */
+const importData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    const createUsers = await User.insertMany(users);
+    const adminUser = createUsers[0]._id;
+    console.log('adminUser', adminUser);
+
+    const sampleProducts = products.map(product => {
+      return {
+        ...product,
+        user: adminUser,
+      };
+    });
+
+    await Product.insertMany(sampleProducts);
+
+    console.log('Data Imported!'.green.inverse);
+    process.exit();
+  } catch (error) {}
+  console.error(`${error}`.red.inverse);
+  process.exit(1);
+};
+
+/* data delete */
+const destroyData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log('Data Destroyed!'.red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+// 시작 시 실행 인자를 받으려면
+if (process.argv[2] === '-d') {
+  destroyData();
+} else {
+  importData();
+}
