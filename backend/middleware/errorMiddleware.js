9cad5d3ec6d85789d5a43fa56c8fb4e1bae72de5
kimbeomjin95 <kimbeomjin95@github.com>
2022-02-01 19:08:55 +0900

    사용자 지정 오류처리

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,17 @@
+// next: 현재 라우터에서 판단하지 않고 다음 라우터로 넘기는것
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorhandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
+};
+
+export { notFound, errorhandler };
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import _ from 'lodash';
 import connectDb from './config/db.js'; // .js파일을 가져올 경우 .js 추가
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorhandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -17,6 +17,10 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+// app.use()는 미들웨어 기능을 마운트하거나 지정된 경로에 마운트하는 데 사용
+app.use(notFound);
+app.use(errorhandler);
 
 // env 환경변수 get
 const PORT = process.env.PORT || 5000;
