4c2c0b8985c359cb5de85432f4c39cd895422605
kimbeomjin95 <kimbeomjin95@github.com>
2022-02-02 13:45:40 +0900

    장바구니 기능 추가

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import Message from '../components/Message';
+import { useLocation, useParams } from 'react-router';
+import { addToCart } from '../actions/cartActions';
 
 const CartScreen = () => {
+  const { id } = useParams();
+  const { search } = useLocation();
+  const dispatch = useDispatch();
+  const qty = search ? Number(search.split('=')[1]) : 1;
+  const cart = useSelector(state => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (id) {
+      dispatch(addToCart(id, qty));
+    }
+  }, [dispatch, id, qty]);
+
   return <div>Cart</div>;
 };
 
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -17,7 +17,7 @@ import Loader from '../components/Loader';
 import Message from '../components/Message';
 
 const ProductScreen = () => {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const productDetailReducer = useSelector(state => state.productDetailReducer);
