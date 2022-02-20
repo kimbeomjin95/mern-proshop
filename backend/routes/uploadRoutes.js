f445a657c0c2bd65d5c02f57519d16cbf80f4a5a
kimbeomjin95 <kimbeomjin95@github.com>
2022-02-20 19:45:24 +0900

    백엔드 이미지 업로드 구성

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,44 @@
+import path from 'path';
+import express from 'express';
+import multer from 'multer';
+
+const router = express.Router();
+
+// 파일등록
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`,
+    ); // path.extname: 파일이름의 확장자를 가져옴
+  },
+});
+
+// 파일 타입 체크
+const checkFileType = (file, cb) => {
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb('이미지형식 파일만 업로드 가능합니다.');
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+router.post('/', upload.single('image'), (req, res) => {
+  res.send(`/${req.file.path}`);
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'; // path 모듈은 폴더와 파일의 경로를 지정해주는 모듈이다.
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDb from './config/db.js'; // .js파일을 가져올 경우 .js 추가
@@ -5,6 +6,7 @@ import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorhandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -24,11 +26,19 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
+
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID),
-),
-  // app.use()는 미들웨어 기능을 마운트하거나 지정된 경로에 마운트하는 데 사용
-  app.use(notFound);
+);
+
+// __dirname 은 현재 실행 중인 폴더 경로
+const __dirname = path.resolve();
+console.log('__dirname', __dirname);
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+// app.use()는 미들웨어 기능을 마운트하거나 지정된 경로에 마운트하는 데 사용
+app.use(notFound);
 app.use(errorhandler);
 
 // env 환경변수 get
