32b34108e094ebedb7b956da641b8b97f7800da6
kimbeomjin95 <kimbeomjin95@github.com>
2022-03-06 12:59:41 +0900

    FOOTER 수정

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,12 +1,47 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
   return (
     <footer>
       <Container>
         <Row>
-          <Col className="text-center py-3">Copyright &copy; Proshop</Col>
+          <Col />
+          <Col md="auto">
+            <p className="text-muted">
+              ㈜ 에이치피물류 | 대표 : 이상열 | 사업자 등록 번호 : 655-88-00171
+              | 도매 및 소매업
+            </p>
+          </Col>{' '}
+          <Col />
+        </Row>
+        <Row>
+          <Col />
+          <Col md="auto">
+            <p className="text-muted">
+              주소 : 경기도 군포시 흥안대로 24-3(금정동) 101호 | E-mail :
+              rcn870107@naver.com
+            </p>
+          </Col>
+          <Col />
+        </Row>
+        <Row>
+          <Col />
+          <Col md="auto">
+            <p className="text-muted">
+              대표전화번호 : 1588-2696 | Fax : 031-476-7525
+            </p>
+          </Col>
+          <Col />
+        </Row>
+        <Row>
+          <Col />
+          <Col md="auto">
+            <p className="text-muted">
+              하이플러스유통 &copy; All rights reserved.
+            </p>
+          </Col>
+          <Col />
         </Row>
       </Container>
     </footer>
